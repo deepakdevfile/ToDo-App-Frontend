04551632635b9ffcaee385e8d62e81072204c8e1
deepakdevfile <deepakdevfile@gmail.com>
2026-08-29 14:45:15 +0530

    C3: Implemented Login and Logout Functionalities

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,58 +1,81 @@
-import React, { useState } from "react";
-import { FaSignInAlt } from "react-icons/fa";
+import { useEffect, useState } from "react"
+import { FaSignInAlt } from "react-icons/fa"
+import { useDispatch, useSelector } from "react-redux"
+import { login, reset } from '../features/auth/authSlice'
+import { useNavigate } from "react-router-dom"
+import Spinner from './Spinner'
+import { toast } from 'react-toastify'
 
 export default function Login(){
     const [formData, setFormData] = useState({ email: '', password: '' });
     const { email, password } = formData;
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const { user, isLoading, isError, isSuccess, message } = useSelector(state => state.auth)
+
+    useEffect(() => {
+        if(isError){
+            toast.error(message)
+        }
+        if(isSuccess || user){
+            navigate('/')
+        }
+        dispatch(reset())
+    }, [user, isError, isSuccess, message, navigate, dispatch])
 
     function onChange(e){
         setFormData(prevState => ({...prevState, [e.target.name]: e.target.value}));
     }
 
     function onSubmit(e){
-        e.preventDefault();
+        e.preventDefault()
+        const userData = { email, password }
+        dispatch(login(userData))
     }
 
     return (
-        <>
-            <section className="heading">
-                <h1>
-                    <FaSignInAlt/>
-                    Login
-                </h1>
-                <p>Login and start creating tasks</p>
-            </section>
-            <section className="form">
-                <form onSubmit={onSubmit}>
-                    <div className="form-group">
-                        <input 
-                            type="email" 
-                            className="form-control"
-                            id="email"
-                            name="email"
-                            value={email}
-                            placeholder="Enter your email"
-                            onChange={onChange}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input 
-                            type="password" 
-                            className="form-control"
-                            id="password"
-                            name="password"
-                            value={password}
-                            placeholder="Enter password"
-                            onChange={onChange}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <button type="submit" className="btn btn-block">
-                            Submit
-                        </button>
-                    </div>
-                </form>
-            </section>
-        </>
+        isLoading ? <Spinner /> : (
+            <>
+                <section className="heading">
+                    <h1>
+                        <FaSignInAlt />
+                        Login
+                    </h1>
+                    <p>Login and start creating tasks</p>
+                </section>
+                <section className="form">
+                    <form onSubmit={onSubmit}>
+                        <div className="form-group">
+                            <input
+                                type="email"
+                                className="form-control"
+                                id="email"
+                                name="email"
+                                value={email}
+                                placeholder="Enter your email"
+                                onChange={onChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="password"
+                                className="form-control"
+                                id="password"
+                                name="password"
+                                value={password}
+                                placeholder="Enter password"
+                                onChange={onChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-block">
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </section>
+            </>
+        )
     )
 }
