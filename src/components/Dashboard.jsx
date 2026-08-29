5493ebb01c0f99917f052748cf2bf82d5a37a7b7
deepakdevfile <deepakdevfile@gmail.com>
2026-08-29 16:00:54 +0530

    C4: Dashboard creation and Task Form

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import { useEffect } from "react"
+import { useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import TaskForm from './TaskForm'
 
 export default function Dashboard(){
+    const { user } = useSelector(state => state.auth)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if(!user){
+            navigate('/login')
+        }
+    }, [user, navigate])
+
     return (
         <>
-            Dashboard
+            <section className='heading'>
+                <h1>Welcome {user && user.name}</h1>
+                <p>Tasks Dashboard</p>
+            </section>
+            <TaskForm />
         </>
     )
 }
